641a4ce861ef93796565448a5e57490f4cc96f86
KY-Ng <raionkurane@gmail.com>
2021-04-12 16:23:26 +0900

    test out jalr calculation

--- src/sand.v
+++ src/sand.v
@@ -1,15 +1,19 @@
 // this module is used for drafing
 
 module sand ();
-    reg [31:0] in;
-    reg [31:0] offset;    
+    reg [31:0] in1;
+    reg [31:0] in2;    
     wire [31:0] out;
 
-    box subject(.in(in), .offset(offset), .out(out));
+    box subject(.in1(in1), .in2(in2), .out(out));
 
     initial begin
-        assign in = 32'd1;
-        assign offset = 32'd8;
+        assign in1 = 32'd1;
+        assign in2 = 32'd8;
+        #10
+
+        assign in1 = 32'd2;
+        assign in2 = 32'd8;
         #10
 
         $finish;
@@ -18,10 +22,10 @@ module sand ();
 endmodule
 
 module box (
-    input [31:0] in,
-    input [31:0] offset,
+    input [31:0] in1,
+    input [31:0] in2,
     output [31:0] out
 );
     
-    assign out = in << offset;
+    assign out = (in1 + in2) & ~1;
 endmodule
