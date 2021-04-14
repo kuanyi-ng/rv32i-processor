a0354800ee89ac7793a986c26d170a9253cd456a
KY-Ng <raionkurane@gmail.com>
2021-04-14 16:23:53 +0900

    add conversion for shift instructions

--- test/test_opcode_to_imm_type.v
+++ test/test_opcode_to_imm_type.v
@@ -1,8 +1,9 @@
 module test_opcode_to_imm_type ();
     reg [6:0] opcode;
+    reg [2:0] funct3;
     wire [2:0] imm_type;
 
-    opcode_to_imm_type subject(.opcode(opcode), .imm_type(imm_type));
+    opcode_to_imm_type subject(.opcode(opcode), .funct3(funct3), .imm_type(imm_type));
 
     initial begin
         // U (LUI)
@@ -35,9 +36,22 @@ module test_opcode_to_imm_type ();
         // expect: 010
         #10
 
-        // I (Arithmetic including shamt)
+        // I (does not include shamt)
         assign opcode = 7'b0010011;
+        assign funct3 = 3'b000;
         // expect: 000
+        #10
+
+        // I (SLLI)
+        assign opcode = 7'b0010011;
+        assign funct3 = 3'b001;
+        // expect: 101
+        #10
+
+        // I (SRLI, SRAI)
+        assign opcode = 7'b0010011;
+        assign funct3 = 3'b101;
+        // expect: 101
         #10
 
         // R
