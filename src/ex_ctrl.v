4cf261dd963e2d73ece5049fcaddcc80b99f3d2a
KY-Ng <raionkurane@gmail.com>
2021-04-15 14:45:13 +0900

    rename function from xx_out to xx_ctrl

--- src/ex_ctrl.v
+++ src/ex_ctrl.v
@@ -9,14 +9,14 @@ module ex_ctrl (
     //
     // main
     //
-    assign a_sel = a_sel_out(opcode);
-    assign b_sel = b_sel_out(opcode);
-    assign branch_alu_op = branch_alu_op_out(opcode, funct3);
+    assign a_sel = a_sel_ctrl(opcode);
+    assign b_sel = b_sel_ctrl(opcode);
+    assign branch_alu_op = branch_alu_op_ctrl(opcode, funct3);
 
     //
     // functions
     //
-    function a_sel_out(input [6:0] opcode);
+    function a_sel_ctrl(input [6:0] opcode);
         // since most instructions use data1,
         // we will only check if the instructions need pc
         reg is_aupic;
@@ -29,22 +29,22 @@ module ex_ctrl (
             is_jal = (opcode == 7'b1101111);
             is_branch = (opcode == 7'b1100011);
 
-            a_sel_out = is_aupic || is_jal || is_branch;            
+            a_sel_ctrl = is_aupic || is_jal || is_branch;
         end
     endfunction
 
-    function b_sel_out(input [6:0] opcode);
+    function b_sel_ctrl(input [6:0] opcode);
         // only R-Type instructions use data2
         reg is_r_type_ir;
 
         begin
             is_r_type_ir = (opcode == 7'b0110011);
 
-            b_sel_out = !is_r_type_ir;
+            b_sel_ctrl = !is_r_type_ir;
         end
     endfunction
 
-    function [2:0] branch_alu_op_out(input [6:0] opcode, input [2:0] funct3);
+    function [2:0] branch_alu_op_ctrl(input [6:0] opcode, input [2:0] funct3);
         // first check if instruction is one of the following:
         // JAL. JALR, Branch
         reg is_jal;
@@ -56,34 +56,34 @@ module ex_ctrl (
             is_jalr = (opcode == 7'b1100111);
             is_branch = (opcode == 7'b1100011);
 
-            if (is_jal || is_jalr) branch_alu_op_out = 3'b110;
+            if (is_jal || is_jalr) branch_alu_op_ctrl = 3'b110;
             // TODO: maybe can make this easier by
-            // branch_alu_op_out = funct3
+            // branch_alu_op_ctrl = funct3
             else if (is_branch) begin
                 case (funct3)
                     // BEQ
-                    3'b000: branch_alu_op_out = 3'b000;
+                    3'b000: branch_alu_op_ctrl = 3'b000;
 
                     // BNE
-                    3'b001: branch_alu_op_out = 3'b001;
+                    3'b001: branch_alu_op_ctrl = 3'b001;
 
                     // BLT
-                    3'b100: branch_alu_op_out = 3'b010;
+                    3'b100: branch_alu_op_ctrl = 3'b010;
 
                     // BGE
-                    3'b101: branch_alu_op_out = 3'b100;
+                    3'b101: branch_alu_op_ctrl = 3'b100;
 
                     // BLTU
-                    3'b110: branch_alu_op_out = 3'b011;
+                    3'b110: branch_alu_op_ctrl = 3'b011;
 
                     // BGEU
-                    3'b111: branch_alu_op_out = 3'b101;
+                    3'b111: branch_alu_op_ctrl = 3'b101;
 
                     // default: 3'b111
-                    default: branch_alu_op_out = 3'b111;
+                    default: branch_alu_op_ctrl = 3'b111;
                 endcase
             end
-            else branch_alu_op_out = 3'b111;
+            else branch_alu_op_ctrl = 3'b111;
         end
     endfunction
 endmodule
