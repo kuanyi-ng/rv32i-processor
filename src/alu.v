5c0ef5117bd806954205341a32889d881e9dfb1d
KY-Ng <raionkurane@gmail.com>
2021-04-15 15:30:08 +0900

    simplify relationship between alu_op and operations in alu

--- src/alu.v
+++ src/alu.v
@@ -19,100 +19,42 @@ module alu (
                 // ADD, ADDI, Load, Store, Branch, JAL, AUPIC
                 4'b0000: alu_out = s_in1 + s_in2;
 
-                // SUB
-                4'b0001: alu_out = s_in1 - s_in2;
-
-                // XOR, XORI
-                4'b0010: alu_out = in1 ^ in2;
-
-                // OR, ORI
-                4'b0011: alu_out = in1 | in2;
-
-                // AND, ANDI
-                4'b0100: alu_out = in1 & in2;
-
                 // SLL, SLLI
-                4'b0101: alu_out = s_in1 << in2;
-
-                // SRL, SRLI
-                4'b0110: alu_out = s_in1 >> in2;
-                
-                // SRA, SRAI
-                4'b0111: alu_out = s_in1 >>> in2;
+                4'b0001: alu_out = s_in1 << in2;
 
                 // SLT, SLTI
-                4'b1000: alu_out = (s_in1 < s_in2) ? 32'd1 : 32'd0;
+                4'b0010: alu_out = (s_in1 < s_in2) ? 32'd1 : 32'd0;
 
                 // SLTU, SLTIU
-                4'b1001: alu_out = (in1 < in2) ? 32'd1 : 32'd0;
+                4'b0011: alu_out = (in1 < in2) ? 32'd1 : 32'd0;
+
+                // XOR, XORI
+                4'b0100: alu_out = in1 ^ in2;
+
+                // SRL, SRLI
+                4'b0101: alu_out = s_in1 >> in2;
+
+                // OR, ORI
+                4'b0110: alu_out = in1 | in2;
+
+                // AND, ANDI
+                4'b0111: alu_out = in1 & in2;
+
+                // SUB
+                4'b1000: alu_out = s_in1 - s_in2;
+
+                // LUI
+                4'b1001: alu_out = in2;
 
                 // JALR
                 4'b1010: alu_out = (in1 + in2) & ~1;
 
-                // LUI
-                4'b1011: alu_out = in2;
+                // SRA, SRAI
+                4'b1101: alu_out = s_in1 >>> in2;
 
                 // default: false
                 default: alu_out = 32'b0;
-            endcase 
-        end        
-    endfunction    
-
-    // // prepare signed inputs
-    // wire signed [31:0] s_in1 = in1;
-    // wire signed [31:0] s_in2 = in2;
-
-    // // whenever either one of in1, in2, alu_op changes,
-    // // this block will be run and compute a new value for out.
-    // always @(in1 or in2 or alu_op) begin
-    //     case (alu_op)
-    //         // ADD
-    //         4'b0000: out <= s_in1 + s_in2;
-
-    //         // SUB
-    //         4'b0001: out <= s_in1 - s_in2;
-
-    //         // XOR
-    //         4'b0010: out <= s_in1 ^ s_in2;
-
-    //         // OR
-    //         4'b0011: out <= s_in1 | s_in2;
-
-    //         // AND
-    //         4'b0100: out <= s_in1 & s_in2;
-
-    //         // SLL
-    //         4'b0101: out <= in1 << in2;
-
-    //         // SRL
-    //         4'b0110: out <= in1 >> in2;
-            
-    //         // SRA
-    //         4'b0111: out <= s_in1 >>> in2;
-
-    //         // EQ
-    //         4'b1000: out <= (in1 == in2) ? 32'b1 : 32'b0;
-
-    //         // NE
-    //         4'b1001: out <= (in1 != in2) ? 32'b1: 32'b0;
-
-    //         // LT
-    //         4'b1010: out <= (s_in1 < s_in2) ? 32'b1 : 32'b0;
-
-    //         // LTU
-    //         4'b1011: out <= (in1 < in2) ? 32'b1 : 32'b0;
-
-    //         // GE
-    //         4'b1100: out <= (s_in1 > s_in2) ? 32'b1 : 32'b0;
-
-    //         // GEU
-    //         4'b1101: out <= (in1 > in2) ? 32'b1 : 32'b0;
-
-    //         // JALR
-    //         4'b1110: out <= (in1 + in2) & ~1;
-
-    //         // default: false
-    //         default: out <= 32'b0;
-    //     endcase
-    // end
+            endcase
+        end
+    endfunction
 endmodule
--- test/test_alu.v
+++ test/test_alu.v
@@ -17,22 +17,22 @@ module test_alu ();
 
         // SUB
         // expect: ffff_fffe
-        assign alu_op = 4'b0001;
+        assign alu_op = 4'b1000;
         #10
 
         // XOR, XORI
         // expect: 0000_0002
-        assign alu_op = 4'b0010;
+        assign alu_op = 4'b0100;
         #10
 
         // OR, ORI
         // expect: 0000_0006
-        assign alu_op = 4'b0011;
+        assign alu_op = 4'b0110;
         #10
 
         // AND, ANDI
         // expect: 0000_0004
-        assign alu_op = 4'b0100;
+        assign alu_op = 4'b0111;
         #10
 
         assign data1 = 32'hf000_0004;
@@ -40,17 +40,17 @@ module test_alu ();
 
         // SLL, SLLI
         // expect: 0000_0040
-        assign alu_op = 4'b0101;
+        assign alu_op = 4'b0001;
         #10
 
         // SRL, SRLI
         // expect: 0f00_0000
-        assign alu_op = 4'b0110;
+        assign alu_op = 4'b0101;
         #10
 
         // SRA, SRAI
         // expect: ff00_0000
-        assign alu_op = 4'b0111;
+        assign alu_op = 4'b1101;
         #10
 
         assign data1 = 32'h0000_1000;
@@ -58,12 +58,12 @@ module test_alu ();
 
         // SLT, SLTI
         // expect: 0000_0000
-        assign alu_op = 4'b1000;
+        assign alu_op = 4'b0010;
         #10
 
         // SLTU, SLTIU
         // expect: 0000_0001
-        assign alu_op = 4'b1001;
+        assign alu_op = 4'b0011;
         #10
 
         // JALR
@@ -80,7 +80,7 @@ module test_alu ();
 
         // LUI
         // expect: 0000_0008
-        assign alu_op = 4'b1011;
+        assign alu_op = 4'b1001;
         #10
 
         $finish;
