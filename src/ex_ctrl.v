b57afb77d37fa63d932bf816c7118eb050f812c3
KY-Ng <raionkurane@gmail.com>
2021-04-15 14:20:22 +0900

    add b_sel control bit circuit in ex_ctrl module

--- src/ex_ctrl.v
+++ src/ex_ctrl.v
@@ -2,12 +2,14 @@ module ex_ctrl (
     input [6:0] opcode,
     input [3:0] funct3,
     input [6:0] funct7,
-    output a_sel            // 0: data1, 1: pc
+    output a_sel,           // 0: data1, 1: pc
+    output b_sel            // 0: data2, 1: imm
 );
     //
     // main
     //
     assign a_sel = a_sel_out(opcode);
+    assign b_sel = b_sel_out(opcode);
 
     //
     // functions
@@ -26,6 +28,17 @@ module ex_ctrl (
             is_branch = (opcode == 7'b1100011);
 
             a_sel_out = is_aupic || is_jal || is_branch;            
+        end
+    endfunction
+
+    function b_sel_out(input [6:0] opcode);
+        // only R-Type instructions use data2
+        reg is_r_type_ir;
+
+        begin
+            is_r_type_ir = (opcode == 7'b0110011);
+
+            b_sel_out = !is_r_type_ir;
         end
     endfunction
 endmodule
--- test/test_ex_ctrl.v
+++ test/test_ex_ctrl.v
@@ -4,62 +4,78 @@ module test_ex_ctrl ();
     reg [6:0] funct7;
 
     wire a_sel;
+    wire b_sel;
 
-    ex_ctrl subject(.opcode(opcode), .funct3(funct3), .funct7(funct7), .a_sel(a_sel));
+    ex_ctrl subject(
+        .opcode(opcode),
+        .funct3(funct3),
+        .funct7(funct7),
+        .a_sel(a_sel),
+        .b_sel(b_sel)
+    );
 
     initial begin
         // LUI
         assign opcode = 7'b0110111;
         // expect:
         // a_sel: 0
+        // b_sel: 1
         #10
 
         // AUPIC
         assign opcode = 7'b0010111;
         // expect:
         // a_sel: 1
+        // b_sel: 1
         #10
 
         // JAL
         assign opcode = 7'b1101111;
         // expect:
         // a_sel: 1
+        // b_sel: 1
         #10
 
         // JALR
         assign opcode = 7'b1100111;
         // expect:
         // a_sel: 0
+        // b_sel: 1
         #10
 
         // Branch
         assign opcode = 7'b1100011;
         // expect:
         // a_sel: 1
+        // b_sel: 1
         #10
 
         // Load
         assign opcode = 7'b0000011;
         // expect:
         // a_sel: 0
+        // b_sel: 1
         #10
 
         // Store
         assign opcode = 7'b0100011;
         // expect:
         // a_sel: 0
+        // b_sel: 1
         #10
 
         // I-Type
         assign opcode = 7'b0010011;
         // expect:
         // a_sel: 0
+        // b_sel: 1
         #10
 
         // R-Type
         assign opcode = 7'b0110011;
         // expect:
         // a_sel: 0
+        // b_sel: 0
         #10
 
         $finish;
