777bdb37414d5045e963a457e69957da400e5065
KY-Ng <raionkurane@gmail.com>
2021-04-19 14:51:25 +0900

    make mem_ctrl to return require_mem_access with value of only 0/1 (no x)

--- src/mem_ctrl.v
+++ src/mem_ctrl.v
@@ -12,15 +12,19 @@ module mem_ctrl (
     //
     assign access_size = access_size_ctrl(opcode, funct3);
     assign write_to_data_mem = write_ctrl(opcode);
-    assign require_mem_access = !data_mem_access_ready_n;
+    assign require_mem_access = mem_access_ctrl(data_mem_access_ready_n, opcode);
 
     //
     // Functions
     //
     function [1:0] access_size_ctrl(input [6:0] opcode, input [2:0] funct3);
+        reg is_load, is_store;
+
         begin
-            if (opcode == 7'b1100011) begin
-                // Branch Instructions
+            assign is_load = (opcode == 7'b1100011) ? 1'b1 : 1'b0;
+            assign is_store = (opcode == 7'b0100011) ? 1'b1 : 1'b0;
+
+            if (is_load) begin
                 case (funct3[1:0])
                     // LB, LBU
                     2'b00: access_size_ctrl = 2'b10;
@@ -36,8 +40,7 @@ module mem_ctrl (
                 endcase
 
                 
-            end else if (opcode == 7'b0100011) begin
-                // Store Instructions
+            end else if (is_store) begin
                 case (funct3)
                     // SB
                     3'b000: access_size_ctrl = 2'b10;
@@ -59,10 +62,32 @@ module mem_ctrl (
     endfunction
 
     function write_ctrl(input [6:0] opcode);
+        reg is_store;
+
         begin
+            assign is_store = (opcode == 7'b0100011) ? 1'b1 : 1'b0;
             // only Store Instructions write to mem
-            write_ctrl = (opcode == 7'b0100011);
+            write_ctrl = is_store;
         end
     endfunction
-    
+
+    function mem_access_ctrl(input mem_access_ready_n, input [6:0] opcode);
+        reg is_load, is_store;
+
+        begin
+            if (mem_access_ready_n) begin
+                // 1: not ready
+                mem_access_ctrl  = 1'b0;
+            end else begin
+                // 0: ready
+                assign is_load = (opcode == 7'b1100011) ? 1'b1 : 1'b0;
+                assign is_store = (opcode == 7'b0100011) ? 1'b1 : 1'b0;
+
+                // request for memory access only for load/store instructions
+                if (is_load) mem_access_ctrl = 1'b1;
+                else if (is_store) mem_access_ctrl = 1'b1;
+                else mem_access_ctrl = 1'b0;
+            end
+        end
+    endfunction
 endmodule
