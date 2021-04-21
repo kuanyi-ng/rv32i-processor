7af6feefeff4f0cef63d2c045a810e032b4204c8
KY-Ng <raionkurane@gmail.com>
2021-04-21 14:51:30 +0900

    try connecting modules in ID stage

--- src/top.v
+++ src/top.v
@@ -60,4 +60,119 @@ module top (
         .out(ir_from_if)
     );
 
+    //
+    // ID
+    //
+
+    wire [4:0] rd1_addr, rd2_addr;
+    wire [4:0] rd_id;
+    wire [6:0] opcode_id;
+    wire [2:0] funct3_id;
+    wire [6:0] funct7_id;
+    wire [31:0] imm_id;
+    id_stage id_stage_inst(
+        .ir(ir_from_if),
+        .rs1(rd1_addr),
+        .rs2(rd2_addr),
+        .rd(rd_id),
+        .opcode(opcode_id),
+        .funct3(funct3_id),
+        .funct7(funct7_id),
+        .imm(imm_id)
+    );
+
+    //
+    // ID-EX
+    //
+
+    wire [31:0] pc_from_id;
+    reg32 pc_id_ex_reg(
+        .clk(clk),
+        .rst_n(rst_n),
+        .in(pc_from_if),
+        .default_in(32'hZ),
+        .out(pc_from_id)
+    );
+
+    wire [31:0] data1_id, data2_id;
+    wire [31:0] data1_from_id, data2_from_id;
+    reg32 data1_id_ex_reg(
+        .clk(clk),
+        .rst_n(rst_n),
+        .in(data1_id),
+        .default_in(32'h0),
+        .out(data1_from_id)
+    );
+    reg32 data2_id_ex_reg(
+        .clk(clk),
+        .rst_n(rst_n),
+        .in(data2_id),
+        .default_in(32'h0),
+        .out(data2_from_id)
+    );
+
+    wire [6:0] funct7_from_id;
+    reg7 funct7_id_ex_reg(
+        .clk(clk),
+        .rst_n(rst_n),
+        .in(funct7_id),
+        .default_in(7'hZ),
+        .out(funct7_from_id)
+    );
+
+    wire [2:0] funct3_from_id;
+    reg3 funct3_id_ex_reg(
+        .clk(clk),
+        .rst_n(rst_n),
+        .in(funct3_id),
+        .default_in(3'hZ),
+        .out(funct3_from_id)
+    );
+
+    wire [4:0] rd_from_id;
+    reg5 rd_id_ex_reg(
+        .clk(clk),
+        .rst_n(rst_n),
+        .in(rd_id),
+        .default_in(5'hZ),
+        .out(rd_from_id)
+    );
+
+    wire [6:0] opcode_from_id;
+    reg7 opcode_id_ex_reg(
+        .clk(clk),
+        .rst_n(rst_n),
+        .in(opcode_id),
+        .default_in(7'hZ),
+        .out(opcode_from_id)
+    );
+
+    wire [31:0] imm_from_id;
+    reg32 imm_id_ex_reg(
+        .clk(clk),
+        .rst_n(rst_n),
+        .in(imm_id),
+        .default_in(32'h0),
+        .out(imm_from_id)
+    );
+
+    //
+    // Register File
+    //
+
+    wire wr_n;
+    wire [4:0] wr_addr;
+    wire [31:0] data_in;
+    rf32x32 regfile_inst(
+        .clk(clk),
+        .reset(rst_n),
+        .wr_n(wr_n),
+        .rd1_addr(rd1_addr),
+        .rd2_addr(rd2_addr),
+        .wr_addr(wr_addr),
+        .data_in(data_in),
+        .data1_out(data1_id),
+        .data2_out(data2_id)
+    );
+
 endmodule
