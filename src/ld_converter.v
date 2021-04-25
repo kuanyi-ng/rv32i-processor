b0c4d952c28e6454b91ac3ac64e0fb3bed61222d
KY-Ng <raionkurane@gmail.com>
2021-04-25 19:08:52 +0900

    rewrite ld_converter to fit how data is actually read from the memory

--- src/ld_converter.v
+++ src/ld_converter.v
@@ -1,28 +1,31 @@
 module ld_converter (
-    input [31:0] in, // data read from memory
-    input [1:0] offset, // addr[1:0]
-    input [2:0] format, // ir[14:12]: funct3
+    input [31:0] in,    // data read from memory
+    input [2:0] funct3,
     output [31:0] out
 );
 
     //
     // Main
     //
-    assign out = prep_ld_data(in, offset, format);
+    assign out = prep_ld_data(in, funct3);
 
     //
     // Functions
     //
-    function [31:0] prep_ld_data(input [31:0] in, input [1:0] offset, input [2:0] format);
+    function [31:0] prep_ld_data(input [31:0] in, input [2:0] funct3);
+        // data read from memory
+        // - size: word => xxxx_xxxx
+        // - size: half => 0000_xxxx
+        // - size: byte => 0000_00xx
         begin
-            case (format)
+            case (funct3)
                 // LB
                 // out: sext(xy) where xy is from { ab, cd, ef, gh }
-                3'b000: prep_ld_data = ld_ext_b(in, offset, 1'b1);
+                3'b000: prep_ld_data = ld_ext_b(in, 1'b1);
 
                 // LH
                 // out: sext(wxyz) where wxyz is from { abcd, efgh }
-                3'b001: prep_ld_data = ld_ext_h(in, offset, 1'b1);
+                3'b001: prep_ld_data = ld_ext_h(in, 1'b1);
 
                 // LW
                 // out: abcd_efgh
@@ -30,11 +33,11 @@ module ld_converter (
 
                 // LBU
                 // out: ext(xy) where xy is from { ab, cd, ef, gh }
-                3'b100: prep_ld_data = ld_ext_b(in, offset, 1'b0);
+                3'b100: prep_ld_data = ld_ext_b(in, 1'b0);
 
                 // LHU
                 // out: ext(wxyz) where wxyz is from { abcd, efgh }
-                3'b101: prep_ld_data = ld_ext_h(in, offset, 1'b0);
+                3'b101: prep_ld_data = ld_ext_h(in, 1'b0);
 
                 // default: in (doesn't perform any operations)
                 default: prep_ld_data = in;
@@ -42,38 +45,25 @@ module ld_converter (
         end
     endfunction
 
-    function [31:0] ld_ext_b(input [31:0] in, input [1:0] offset, input s_ext);
+    function [31:0] ld_ext_b(input [31:0] in, input s_ext);
         reg [7:0] data;
 
         begin
-            // extract one byte of data from in
-            case (offset)
-                2'b00: data = in[7:0];
-                2'b01: data = in[15:8];
-                2'b10: data = in[23:16];
-                2'b11: data = in[31:24];
-                // doesn't require default as every case is covered.
-            endcase
+            assign data = in[7:0];
 
-            // extend extracted data to 32 bits
-            ld_ext_b = (s_ext == 1'b1) ? { { 24{data[7]} }, data } : { 24'b0, data };
+            if (s_ext) ld_ext_b = { { 24{data[7]} }, data };
+            else ld_ext_b = { 24'b0, data };
         end
     endfunction
 
-    function [31:0] ld_ext_h(input [31:0] in, input [1:0] offset, input s_ext);
+    function [31:0] ld_ext_h(input [31:0] in, input s_ext);
         reg [15:0] data;
 
         begin
-            // extract half word of data from in
-            case (offset)
-                2'b00: data = in[15:0];
-                2'b10: data = in[31:16];
-                // default: in[15:0] for the moment (TODO)
-                default: data = in[15:0];
-            endcase
-            
-            // extend extracted data to 32 bits
-            ld_ext_h = (s_ext == 1'b1) ? { { 16{data[15]} }, data } : { 16'b0, data };
+            assign data = in[15:0];
+
+            if (s_ext) ld_ext_h = { { 16{data[15]} }, data };
+            else ld_ext_h = { { 16'b0 }, data };
         end
     endfunction
 endmodule
--- test/test_ld_converter.v
+++ test/test_ld_converter.v
@@ -1,11 +1,10 @@
 module test_ld_converter ();
     reg [31:0] mem_data;
-    reg [1:0] offset;
     reg [2:0] funct3;
 
     wire [31:0] loaded_data;
 
-    ld_converter subject(.in(mem_data), .offset(offset), .format(funct3), .out(loaded_data));
+    ld_converter subject(.in(mem_data), .funct3(funct3), .out(loaded_data));
 
     initial begin
         // data from memory
@@ -16,89 +15,29 @@ module test_ld_converter ();
         assign funct3 = 3'b000;
         // offset 0
         // expect: ffff_ffb4
-        assign offset = 2'b00;
         #10
-        // offset 1
-        // expect: ffff_ffa3
-        assign offset = 2'b01;
-        #10
-        // offset 2
-        // expect: ffff_ff92
-        assign offset = 2'b10;
-        #10
-        // offset 3
-        // expect: ffff_ff81
-        assign offset = 2'b11;
-        #10
-
 
         // LBU
         assign funct3 = 3'b100;
         // offset 0
         // expect: 0000_00b4
-        assign offset = 2'b00;
-        #10
-        // offset 1
-        // expect: 0000_00a3
-        assign offset = 2'b01;
-        #10
-        // offset 2
-        // expect: 0000_0092
-        assign offset = 2'b10;
-        #10
-        // offset 3
-        // expect: 0000_0081
-        assign offset = 2'b11;
         #10
 
         // LH
         assign funct3 = 3'b001;
         // offset 0
         // expect: ffff_a3b4
-        assign offset = 2'b00;
-        #10
-        // offset 1
-        // expect: ffff_a3b4 (TODO)
-        assign offset = 2'b01;
-        #10
-        // offset 2
-        // expect: ffff_8192
-        assign offset = 2'b10;
-        #10
-        // offset 3
-        // expect: ffff_a3b4 (TODO)
-        assign offset = 2'b11;
         #10
 
         // LHU
         assign funct3 = 3'b101;
         // offset 0
         // expect: 0000_a3b4
-        assign offset = 2'b00;
-        #10
-        // offset 1
-        // expect: 0000_a3b4 (TODO)
-        assign offset = 2'b01;
-        #10
-        // offset 2
-        // expect: 0000_8192
-        assign offset = 2'b10;
-        #10
-        // offset 3
-        // expect: 0000_a3b4 (TODO)
-        assign offset = 2'b11;
         #10
 
         // LW
         assign funct3 = 3'b010;
         // expect: 8192_a3b4
-        assign offset = 2'b00;
-        #10
-        assign offset = 2'b01;
-        #10
-        assign offset = 2'b10;
-        #10
-        assign offset = 2'b11;
         #10
 
         $finish;
