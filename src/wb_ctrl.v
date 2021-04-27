c38832bf41b46733591ae99204db0ef861ee9398
KY-Ng <raionkurane@gmail.com>
2021-04-27 15:39:14 +0900

    remove generation of wr_n from wb_stage

--- src/wb_ctrl.v
+++ src/wb_ctrl.v
@@ -1,39 +1,16 @@
 module wb_ctrl (
     input [6:0] opcode,
-    output write_to_reg,
     output [1:0] data_to_reg_sel
 );
     
     //
     // Main
     //
-    assign write_to_reg = write_to_reg_ctrl(opcode);
     assign data_to_reg_sel = data_to_reg_ctrl(opcode);
 
     //
     // Functions
     //
-    function write_to_reg_ctrl(input [6:0] opcode);
-        // whitelist instead of blacklist to be more secure.
-        reg is_lui, is_auipc, is_i_type, is_r_type, is_load, is_jal, is_jalr;
-
-        begin
-            is_lui = (opcode == 7'b0110111);
-            is_auipc = (opcode == 7'b0010111);
-            is_i_type = (opcode == 7'b0010011);
-            is_r_type = (opcode == 7'b0110011);
-            is_load = (opcode == 7'b0000011);
-            is_jal = (opcode == 7'b1101111);
-            is_jalr = (opcode == 7'b1100111);
-
-            if (is_lui || is_auipc || is_i_type || is_r_type || is_load || is_jal || is_jalr) begin
-                write_to_reg_ctrl = 1'b1;
-            end else begin
-                write_to_reg_ctrl = 1'b0;
-            end
-        end
-    endfunction
-
     function [1:0] data_to_reg_ctrl(input [6:0] opcode);
         reg is_load, is_jal, is_jalr;
 
