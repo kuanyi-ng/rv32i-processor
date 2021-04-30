7cf46ee5a8e19be382d85feebbaaf38fb21bd5d4
KY-Ng <raionkurane@gmail.com>
2021-04-30 16:45:44 +0900

    edit shell script for simulation running by adding gui option

--- script/run_simulation.sh
+++ script/run_simulation.sh
@@ -2,6 +2,7 @@
 
 cd test_ground/
 
-xmverilog -s +access+rwc +gui top_test.v +tcl+shm.tcl &
+xmverilog -s +access+rwc top_test.v +tcl+shm.tcl
+xmverilog -s +access+rwc +gui top_test.v +tcl+shm.tcl
 
 cd ~/sandbox
