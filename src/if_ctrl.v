9dda9cb400f8576c6380d61763d5abef422c3970
KY-Ng <raionkurane@gmail.com>
2021-05-04 13:35:12 +0900

    rename jump_or_branch to jump

--- src/if_ctrl.v
+++ src/if_ctrl.v
@@ -1,12 +1,12 @@
 module if_ctrl (
     input [31:0] pc4,
     input [31:0] c,
-    input jump_or_branch,
+    input jump,
     output [31:0] next_pc
 );
     
     //
     // Main
     //
-    assign next_pc = (jump_or_branch == 1'b1) ? c : pc4;
+    assign next_pc = (jump == 1'b1) ? c : pc4;
 endmodule
