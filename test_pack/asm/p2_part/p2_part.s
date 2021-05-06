aebd963e88ddceb69aa8642d7389769510894c70
KY-Ng <raionkurane@gmail.com>
2021-05-06 14:38:39 +0900

    update test_pack from senpai

--- test_pack/asm/p2_part/p2_part.s
+++ test_pack/asm/p2_part/p2_part.s
@@ -2,131 +2,36 @@
 main:
 	li	x16,	0x80010000
 
-	lb	x8,	0x0(x16)
-	lb	x9,	0x1(x16)
-	add	x10,	x8,	x9
-	sh	x10,	0x2(x16)
-
-	addi	x10,	x10,	-1
-	sw	x10,	0x4(x16)
-
-	lb	x8,	0x8(x16)
-	lbu	x9,	0x9(x16)
-	add	x10,	x8,	x9
-	sh	x10,	0xa(x16)
-
-	addi	x10,	x10,	-11
-	sw	x10,	0xc(x16)
-	
-	lhu	x8,	0x10(x16)
-	lh	x9,	0x12(x16)
-	sub	x10,	x8,	x9
-	sw	x10,	0x14(x16)
-
-	lh	x8,	0x18(x16)
-	lb	x9,	0x1a(x16)
-	sub	x10,	x8,	x9
-	sb	x10,	0x1b(x16)
-
-	lh	x8,	0x1c(x16)
-	lh	x9,	0x1e(x16)
-	and	x10,	x8,	x9
-	sw	x10,	0x20(x16)
-	
-	andi	x10,	x10,	0xffffff00
-	sw	x10,	0x24(x16)
-
-	lhu	x8,	0x1c(x16)
-	lhu	x9,	0x1e(x16)
-	or	x10,	x8,	x9
-	sw	x10,	0x28(x16)
-	li	x30, 	0xfffff00f
-	or	x10,	x10,	x30
-	sw	x10,	0x2c(x16)
-
-	lh	x8,	0x1c(x16)
-	lhu	x9,	0x1e(x16)
-	xor	x10,	x8,	x9
-	sw	x10,	0x30(x16)
-
-	xori	x10,	x10,	0xffffff00
-	sw	x10,	0x34(x16)
-
-	lhu	x8,	0x1c(x16)
-	lhu	x9,	0x1e(x16)
-	or	x10,	x8,	x9
-	xori x10, x10, 0xffffffff
-	sw	x10,	0x38(x16)
-
-	lw	x8,	0x3c(x16)
-	slli	x10,	x8,	4
-	sw	x10,	0x40(x16)
-
-	srli	x10,	x8,	4
-	sw	x10,	0x44(x16)
-
-	srai	x10,	x8,	4
-	sw	x10,	0x48(x16)
-
-	lh	x8,	0x3e(x16)
-	srai	x10,	x8,	4
-	sw	x10,	0x4c(x16)
-
-	lw	x8,	0x3c(x16)
-	xor	x9,	x9,	x9
-	ori	x9,	x9,	4
-	sll	x10,	x8,	x9
-	sw	x10,	0x50(x16)
-
-	srl	x10,	x8,	x9
-	sw	x10,	0x54(x16)
-
-	sra	x10,	x8,	x9
-	sw	x10,	0x58(x16)
-
-	lh	x8,	0x3e(x16)
-	sra	x10,	x8,	x9
-	sw	x10,	0x5c(x16)
-
 	lb	x8,	0x60(x16)
 	lb	x9,	0x61(x16)
-	slt	x10,	x8,	x8
-	sb	x10,	0x64(x16)
-	sltu	x10,	x8,	x8
-	sb	x10,	0x65(x16)
-	slt	x10,	x8,	x9
-	sb	x10,	0x66(x16)
 	sltu	x10,	x8,	x9
-	sb	x10,	0x67(x16)
-	lb	x8,	0x62(x16)
-	slt	x10,	x8,	x9
-	sb	x10,	0x68(x16)
-	sltu	x10,	x8,	x9
-	sb	x10,	0x69(x16)
-	lb	x9,	0x63(x16)
-	slt	x10,	x8,	x9
-	sb	x10,	0x6a(x16)
-	sltu	x10,	x8,	x9
-	sb	x10,	0x6b(x16)
-
 	xor	x10,	x10,	x10
-	sw	x10,	0x6c(x16)
-	sw	x10,	0x70(x16)
-	sw	x10,	0x74(x16)
-	sw	x10,	0x78(x16)
-	sw	x10,	0x7c(x16)
-	sw	x10,	0x80(x16)
-	sw	x10,	0x84(x16)
 	ori	x10,	x10,	0x1
-	j	L1
+
+    # =====
+    # x8: ffff_fff5 (-11)
+    # x9: ffff_fff3 (-13)
+    # x10: 0000_0001
+    # =====
+
+	j	L32
 	sb	x10,	0x6c(x16)
 L1:
 	jal	L2
+    # jump to L2
+    # x31: addr [ sb x10, 0x6d(x16) ]
 	sb	x10,	0x6d(x16)
-	ret
+	jr	x31
+    # jump to addr [ xor x8, x8, x8 ]
 L2:
 	ori	x10,	x10,	0x3
-	jalr	x1
+	jalr	x31
+    # jump to addr [ sb x10, 0x6d(x16) ]
+    # x31: addr [ xor x8, x8, x8 ]
+
+    # =====
+    # x10: 0000_0003
+    # =====
 L3:
 	xor	x8,	x8,	x8
 	addi	x8,	x8,	0x03
@@ -134,66 +39,164 @@ L3:
 	addi	x9,	x9,	-1
 	beq	x8,	x8,	L4
 	sb	x10,	0x6e(x16)
+
+    # =====
+    # x8: 0000_0003
+    # x9: ffff_ffff
+    # branch to L4
+    # =====
 L4:
 	beq	x8,	x9,	L5
 	sb	x10,	0x6f(x16)
+
+    # =====
+    # doesn't branch to L5
+    # =====
+
 L5:
 	bne	x8,	x8,	L6
 	sb	x10,	0x70(x16)
+
+    # =====
+    # doesn't branch to L6
+    # =====
+
 L6:
 	bne	x8,	x9,	L7
 	sb	x10,	0x71(x16)
+
+    # =====
+    # branch to L7
+    # =====
+
 L7:
+    # branch <= 0
 	blez	x8,	L8
 	sb	x10,	0x72(x16)
+
+    # =====
+    # doesn't branch to L8
+    # =====
+
 L8:
 	blez	x0,	L9
 	sb	x10,	0x73(x16)
+
+    # =====
+    # branch to L9
+    # =====
+
 L9:
 	blez	x9,	L10
 	sb	x10,	0x74(x16)
+
+    # =====
+    # branch to L10
+    # =====
+
 L10:
+    # branch > 0
 	bgtz	x8,	L11
 	sb	x10,	0x75(x16)
+
+    # =====
+    # branch to L11
+    # =====
+
 L11:
 	bgtz	x0,	L12
 	sb	x10,	0x76(x16)
+
+    # =====
+    # doesn't branch to L12
+    # =====
+
 L12:
 	bgtz	x9,	L13
 	sb	x10,	0x77(x16)
+    
+    # =====
+    # doesn't branch to L13
+    # =====
+
 L13:
+    # branch >= 0
 	bgez	x8,	L14
 	sb	x10,	0x78(x16)
+
+    # =====
+    # branch to L14
+    # =====
+
 L14:
 	bgez	x0,	L15
 	sb	x10,	0x79(x16)
+
+    # =====
+    # branch to L15
+    # =====
+
 L15:
 	bgez	x9,	L16
 	sb	x10,	0x7a(x16)
+
+    # =====
+    # doesn't branch to L16
+    # =====
+
 L16:
+    # branch < 0
 	bltz	x8,	M1
 	jal	L17
+
+    # =====
+    # doesn't branch to M1
+    # jump to L17
+    # =====
+
 M1:
 	sb	x10,	0x7b(x16)
 L17:
 	bltz	x0,	M2
 	jal	L18
+
+    # =====
+    # doesn't branch to M2
+    # jump to L18
+    # =====
+
 M2:
 	sb	x10,	0x7c(x16)
 L18:
 	bltz	x9,	M3
 	jal L19
+
+    # =====
+    # branch to M3
+    # =====
+
 M3:
 	sb	x10,	0x7d(x16)
 L19:
 	bltz	x0,	M4
 	jal L20
+
+    # =====
+    # doesn't branch to M4
+    # jump to L20
+    # =====
+
 M4:
 	sb	x10,	0x7e(x16)
 	j	L21
 L20:
 	ori	x10,	x10,	0x7
-	ret
+	jr	x31
+
+    # =====
+    # x10: 0000_0007
+    # =====
+
 L21:
 	bltz	x8,	L22
 	sb	x10,	0x7f(x16)
@@ -226,12 +229,12 @@ M8:
 	j	L29
 L28:
 	ori	x10,	x10,	0xf
-	ret
+	jr	x31
 L29:
 	jal	L30
 	j	L31
 L30:
-	ret
+	jr	x31
 L31:
 	ori	x10,	x10,	0x1f
 	sb	x10,	0x86(x16)
