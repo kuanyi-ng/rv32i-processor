5b705c9c070c51f5132a95125bc870ce5c4f1f4c
KY-Ng <raionkurane@gmail.com>
2021-05-16 19:43:02 +0900

    Fix unsigned to signed assignment occurs. (VER-318) in branch_alu

--- src/branch_alu.v
+++ src/branch_alu.v
@@ -15,19 +15,13 @@ module branch_alu (
     // functions
     //
     function branch_alu_out(input [31:0] in1, input [31:0] in2, input [2:0] op);
-        reg signed [31:0] s_in1;
-        reg signed [31:0] s_in2;
-
         begin
-            s_in1 = in1;
-            s_in2 = in2;
-
             case (op)
                 // EQ
-                3'b000: branch_alu_out = (in1 == in2) ? 1'b1 : 1'b0;
+                3'b000: branch_alu_out = (in1 == in2);
 
                 // NE
-                3'b001: branch_alu_out = (in1 != in2) ? 1'b1: 1'b0;
+                3'b001: branch_alu_out = (in1 != in2);
 
                 // JAL, JALR
                 3'b010: branch_alu_out = 1'b1;
@@ -36,16 +30,16 @@ module branch_alu (
                 3'b011: branch_alu_out = 1'b0;
 
                 // LT
-                3'b100: branch_alu_out = (s_in1 < s_in2) ? 1'b1 : 1'b0;
+                3'b100: branch_alu_out = ($signed(in1) < $signed(in2));
 
                 // GE
-                3'b101: branch_alu_out = (s_in1 >= s_in2) ? 1'b1 : 1'b0;
+                3'b101: branch_alu_out = ($signed(in1) >= $signed(in2));
 
                 // LTU
-                3'b110: branch_alu_out = (in1 < in2) ? 1'b1 : 1'b0;
+                3'b110: branch_alu_out = (in1 < in2);
 
                 // GEU
-                3'b111: branch_alu_out = (in1 >= in2) ? 1'b1 : 1'b0;
+                3'b111: branch_alu_out = (in1 >= in2);
 
                 // no default case as every case is covered
             endcase 
