48f4b91fd70e94c42d69816e84063db2c53891c5
KY-Ng <raionkurane@gmail.com>
2021-05-19 15:59:37 +0900

    take in opcode_from_ex as parameter for interlock_u

--- src/interlock_u.v
+++ src/interlock_u.v
@@ -6,6 +6,7 @@
 module interlock_u (
     input imem_ack_n,   // 0: succeed, 1: still accessing
     input dmem_ack_n,   // 0: succeed, 1: still accessing
+    input [6:0] opcode, // opcode from MEM Stage
 
     output interlock
 );
@@ -14,6 +15,23 @@ module interlock_u (
     // Main
     //
 
-    assign interlock = imem_ack_n || dmem_ack_n;
+    assign interlock = imem_ack_n || dmem_interlock_ctrl(dmem_ack_n, opcode);
+
+    // Function
+    localparam [6:0] load = 7'b0000011;
+    localparam [6:0] store = 7'b0100011;
+
+    function dmem_interlock_ctrl(input dmem_ack_n, input [6:0] opcode);
+        reg is_load, is_store;
+
+        begin
+            is_load = (opcode == load);
+            is_store = (opcode == store);
+
+            // only interlock when memory access doesn't complete
+            // for Load or Store instructions
+            dmem_interlock_ctrl = (is_load || is_store) && dmem_ack_n;
+        end
+    endfunction
     
 endmodule
--- test/test_interlock_u.v
+++ test/test_interlock_u.v
@@ -1,15 +1,19 @@
 module test_interlock_u ();
     reg imem_ack_n, dmem_ack_n;
+    reg [6:0] opcode;
 
     wire interlock;
 
     interlock_u subject(
         .imem_ack_n(imem_ack_n),
         .dmem_ack_n(dmem_ack_n),
+        .opcode(opcode),
         .interlock(interlock)
     );
 
     initial begin
+        // Load
+        assign opcode = 7'b0000011;
         assign imem_ack_n = 0;
         assign dmem_ack_n = 0;
         // interlock: 0
@@ -18,6 +22,50 @@ module test_interlock_u ();
         assign imem_ack_n = 0;
         assign dmem_ack_n = 1;
         // interlock: 1
+        #5
+
+        assign imem_ack_n = 1;
+        assign dmem_ack_n = 0;
+        // interlock: 1
+        #5
+        
+        assign imem_ack_n = 1;
+        assign dmem_ack_n = 1;
+        // interlock: 1
+        #5
+
+        // Store
+        assign opcode = 7'b0100011;
+        assign imem_ack_n = 0;
+        assign dmem_ack_n = 0;
+        // interlock: 0
+        #5
+
+        assign imem_ack_n = 0;
+        assign dmem_ack_n = 1;
+        // interlock: 1
+        #5
+
+        assign imem_ack_n = 1;
+        assign dmem_ack_n = 0;
+        // interlock: 1
+        #5
+        
+        assign imem_ack_n = 1;
+        assign dmem_ack_n = 1;
+        // interlock: 1
+        #5
+
+        // Others (R)
+        assign opcode = 7'b0110011;
+        assign imem_ack_n = 0;
+        assign dmem_ack_n = 0;
+        // interlock: 0
+        #5
+
+        assign imem_ack_n = 0;
+        assign dmem_ack_n = 1;
+        // interlock: 0
         #5
 
         assign imem_ack_n = 1;
