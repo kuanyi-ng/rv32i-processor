9915f231473a1e102a0e257ecc7bf9eedfecf9be
KY-Ng <raionkurane@gmail.com>
2021-05-24 15:58:53 +0900

    add docker image build command into local_env script

--- script/local_env.sh
+++ script/local_env.sh
@@ -2,4 +2,8 @@
 
 DOCKER_IMAGE=ubuntu-riscv-env
 
+# Build Docker Image
+docker build . -t $DOCKER_IMAGE:latest
+
+# Enter Docker Container
 docker run -it --rm --name rv32-env $DOCKER_IMAGE /bin/bash
