b519ea7f05c9466b1f4d5fb4014a5c244043cbbc
KY-Ng <raionkurane@gmail.com>
2021-05-24 15:57:49 +0900

    add test_csrs (probably completed)

--- test/test_csrs.v
+++ test/test_csrs.v
@@ -43,14 +43,87 @@ module test_csrs ();
     initial begin
         // Reset
         rst_n = 1'b0;
-        #10
+        #5
 
         rst_n = 1'b1;
+        #5
+
+        // Write to Read-only CSRs
+        // shouldn't be update
+        wr_csr_n = 1'b0;
+        csr_data_in = 32'hffff_ffff;
+
+        csr_wr_addr = mvendorid_addr;
+        #10
+
+        csr_wr_addr = marchid_addr;
+        #10
+
+        csr_wr_addr = mimpid_addr;
+        #10
+
+        csr_wr_addr = mhartid_addr;
+        #10
+
+        csr_wr_addr = misa_addr;
+        #10
+
+        csr_wr_addr = mtvec_addr;
+        #10
+
+        csr_wr_addr = mcounteren_addr;
+        #10
+
+        // Read Read-only CSRs
+        // check if they are not update
+        wr_csr_n = 1'b1;
+
+        csr_addr = mvendorid_addr;
+        // 32'b0
+        #10
+
+        csr_addr = marchid_addr;
+        // 32'b0
+        #10
+
+        csr_addr = mimpid_addr;
+        // 32'b0
+        #10
+
+        csr_addr = mhartid_addr;
+        // 32'b0
+        #10
+
+        csr_addr = misa_addr;
+        // b'01_0000_00_00_00_00_00_00_00_00_01_00_00_00_00
+        // h'4000_0100
+        #10
+
+        csr_addr = mtvec_addr;
+        // h'0000_0004
+        #10
+
+        csr_addr = mcounteren_addr;
+        // 32'b0
+        #10
+
+        // Write to mstatus
+        wr_csr_n = 1'b0;
+
+        csr_wr_addr = mstatus_addr;
+        csr_data_in = 32'hffff_ffff;
+        #10
+
+        // Read mstatus
+        wr_csr_n = 1'b1;
+
+        csr_addr = mstatus_addr;
+        // 0_00000000_000000_00_00_11_00_11_0_111_0_11
+        // h'0000_19bb
         #10
 
         // Write Reg32
         wr_csr_n = 1'b0;
-        #10
 
         csr_wr_addr = mscratch_addr;
         csr_data_in = 32'h1234_5678;
@@ -72,33 +145,55 @@ module test_csrs ();
         wr_csr_n = 1'b1;
 
         csr_addr = mscratch_addr;
+        // h'1234_5678
         #10
 
         csr_addr = mepc_addr;
+        // h'9abc_efec
         #10
 
         csr_addr = mcause_addr;
+        // h'ba98_7654
         #10
 
         csr_addr = mtval_addr;
+        // h'3210_1234
         #10
 
         // Write mie, mip
         wr_csr_n = 1'b0;
 
         csr_wr_addr = mie_addr;
-        csr_data_in = { 20'bx, 12'b1_0_111_0_111_0_11 };
+        csr_data_in = { 20'hf, 12'b1_0_111_0_111_0_11 };
         #10
 
         csr_wr_addr = mip_addr;
-        csr_data_in = { 20'bx, 12'b1_0_101_0_101_0_11 };
+        csr_data_in = { 20'hf, 12'b1_0_101_0_101_0_11 };
+        #10
+
+        // Read MIE, MIP
+        wr_csr_n = 1'b1;
+
+        csr_addr = mie_addr;
+        // h'0000_0bbb
+        #10
+
+        csr_addr = mip_addr;
+        // h'0000_0aab
         #10
 
         $finish;
     end
 
     initial begin
-        $monitor("t: %3d, csr_value: %h", $time, csr_out);
+        $monitor(
+            "t: %3d, csr_addr: %h, csr_value: %h, csr_wr_addr: %h, csr_data_in: %h",
+            $time,
+            csr_addr,
+            csr_out,
+            csr_wr_addr,
+            csr_data_in
+        );
     end
 
 endmodule
