5d46faf418c4ce6fd446da46658a6d0d029c59dd
KY-Ng <raionkurane@gmail.com>
2021-05-26 16:30:23 +0900

    add parameters to mem_ctrl

--- src/mem_stage.v
+++ src/mem_stage.v
@@ -2,7 +2,18 @@
 `include "st_converter.v"
 `include "ld_converter.v"
 
-module mem_stage (
+module mem_stage #(
+    parameter [6:0] LUI_OP = 7'b0110111,
+    parameter [6:0] AUIPC_OP = 7'b0010111,
+    parameter [6:0] JAL_OP = 7'b1101111,
+    parameter [6:0] JALR_OP = 7'b1100111,
+    parameter [6:0] BRANCH_OP = 7'b1100011,
+    parameter [6:0] LOAD_OP = 7'b0000011,
+    parameter [6:0] STORE_OP = 7'b0100011,
+    parameter [6:0] I_TYPE_OP = 7'b0010011,
+    parameter [6:0] R_TYPE_OP = 7'b0110011,
+    parameter [6:0] CSR_OP = 7'b1110011
+) (
     // inputs from Memory Module
     input data_mem_ready_n,  // 0: ready to access memory, 1: not ready
     input [31:0] data_from_mem,
@@ -23,11 +34,23 @@ module mem_stage (
     output [31:0] data_to_mem
 );
 
+    // NOTE: same as the lower 2 bits of funct3
+    localparam [1:0] WORD = 2'b00;
+    localparam [1:0] HALF = 2'b01;
+    localparam [1:0] BYTE = 2'b10;
+    localparam [1:0] INPROPER_SIZE = 2'b11;
+
     // NOTE: need to request for memory access first,
     // then only we will receive data_mem_ready_n from the memory.
 
-    // TODO: need to think about how to handle data_mem_ready_n
-    mem_ctrl mem_ctrl_inst(
+    mem_ctrl #(
+        .LOAD_OP(LOAD_OP),
+        .STORE_OP(STORE_OP),
+        .WORD(WORD),
+        .HALF(HALF),
+        .BYTE(BYTE),
+        .INPROPER_SIZE(INPROPER_SIZE)
+    ) mem_ctrl_inst(
         .opcode(opcode),
         .funct3(funct3),
         .flush(flush),
