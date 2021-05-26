bd4e14eb5f847ceca67f29b31d74b0e61503d5ce
KY-Ng <raionkurane@gmail.com>
2021-05-26 13:39:31 +0900

    update csr_forward_u to return forward_z based on info from mem stage too

--- src/csr_forward_u.v
+++ src/csr_forward_u.v
@@ -7,14 +7,27 @@ module csr_forward_u (
     input [6:0] opcode_in_ex,
     input wr_csr_n_in_ex,
 
-    output forward_z
+    // Inputs from MEM Stage (current cycle)
+    input [11:0] csr_addr_in_mem,
+    input [6:0] opcode_in_mem,
+    input wr_csr_n_in_mem,
+
+    output [1:0] forward_z
 );
     
     //
     // Main
     //
 
-    assign forward_z = forward_z_ctrl(csr_addr_in_id, csr_addr_in_ex, opcode_in_ex, wr_csr_n_in_ex);
+    assign forward_z = forward_z_ctrl(
+        csr_addr_in_id,
+        csr_addr_in_ex,
+        opcode_in_ex,
+        wr_csr_n_in_ex,
+        csr_addr_in_mem,
+        opcode_in_mem,
+        wr_csr_n_in_mem
+    );
 
     //
     // Function
@@ -22,17 +35,30 @@ module csr_forward_u (
 
     localparam [6:0] csr_op = 7'b1110011;
 
-    function forward_z_ctrl(
+    function [1:0] forward_z_ctrl(
         input [11:0] csr_addr_in_id,
         input [11:0] csr_addr_in_ex,
         input [6:0] opcode_in_ex,
-        input wr_csr_n_in_ex
+        input wr_csr_n_in_ex,
+        input [11:0] csr_addr_in_mem,
+        input [6:0] opcode_in_mem,
+        input wr_csr_n_in_mem
     );
+
+        reg csr_updated_by_prev, csr_updated_by_prev_prev;
         
         begin
+            csr_updated_by_prev = (!wr_csr_n_in_ex) && (opcode_in_ex == csr_op) && (csr_addr_in_id == csr_addr_in_ex);
+            csr_updated_by_prev_prev = (!wr_csr_n_in_mem) && (opcode_in_mem == csr_op) && (csr_addr_in_id == csr_addr_in_mem);
+
             // Forward calculated result to the next CSRs instruction
             // if the current instruction is a CSR instruction
-            forward_z_ctrl = (!wr_csr_n_in_ex) && (opcode_in_ex == csr_op) && (csr_addr_in_id == csr_addr_in_ex);
+
+            // if csr is updated by both instructions (prev and prev_prev)
+            // forward the value updated by prev instruction as it's the latest value.
+            if (csr_updated_by_prev) forward_z_ctrl = 2'b01;
+            else if (csr_updated_by_prev_prev) forward_z_ctrl = 2'b10;
+            else forward_z_ctrl = 2'b00;
         end
     endfunction
 endmodule
