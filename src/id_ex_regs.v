f5251065978b8324c9201448bf0ab6e9d7f40d78
KY-Ng <raionkurane@gmail.com>
2021-05-26 18:24:15 +0900

    Fix: complicated check in if statements of always;

--- src/id_ex_regs.v
+++ src/id_ex_regs.v
@@ -61,10 +61,27 @@ module id_ex_regs (
     reg wr_csr_n;
     reg flush;
 
+    wire stall_or_interlock = stall || interlock;
+
     always @(posedge clk or negedge rst_n) begin
-        if (!rst_n || stall || interlock) begin
+        if (!rst_n) begin
             // reset
-            //
+            pc <= 32'bx;
+            pc4 <= 32'bx;
+            data1 <= 32'bx;
+            data2 <= 32'bx;
+            funct7 <= 7'bx;
+            funct3 <= 3'bx;
+            rs2 <= 5'bx;
+            rd <= 5'bx;
+            csr_addr <= 12'bx;
+            opcode <= 7'bx;
+            imm <= 32'bx;
+            z_ <= 32'bx;
+            wr_reg_n <= 1'b1;   // default not to write
+            wr_csr_n <= 1'b1;   // default not to write
+            flush <= 1'b0;      // default not to flush
+        end else if (stall_or_interlock) begin
             // stall has the same behaviour as reset
             // as a way to prevent double detection of
             // pipeline stall.
