4e923d8d2a3d177429afbff1ad8748abc31cd274
KY-Ng <raionkurane@gmail.com>
2021-05-27 15:14:14 +0900

    update asm/skel/test.s to RISC-V syntax

--- test_pack/asm/skel/test.s
+++ test_pack/asm/skel/test.s
@@ -1,7 +1,10 @@
 	.text    # text section
 main:
-        # main program
-	lui	$8,	0xff00
-	sw	$0,	0($8)
+    # main program
+
+loop:
+    li  x30, 0xff000000
+    sw  x0, 0x0(x30)
+    beq x0, x0, loop
 
 	.data    # data section
