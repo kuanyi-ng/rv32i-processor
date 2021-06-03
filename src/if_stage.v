70c7d719c81ddbcc826fc47d3cb5c0919aed7b0b
KY-Ng <raionkurane@gmail.com>
2021-06-03 14:55:36 +0900

    output i_addr_misaligned from if_stage

--- src/if_stage.v
+++ src/if_stage.v
@@ -5,8 +5,10 @@ module if_stage (
     input [31:0] current_pc,
     input [31:0] c,
     input jump,
+
     output [31:0] pc4,
-    output [31:0] next_pc
+    output [31:0] next_pc,
+    output i_addr_misaligned
 );
 
     pc_adder if_pc_adder(
@@ -21,4 +23,11 @@ module if_stage (
         .next_pc(next_pc)
     );
     
+    // Detection for Instruction Address Misalignment
+    //
+    // Instructions are stored in Imem with address of mulitple of 4.
+    // Allowed value for lower 2 bits of instruction address are { 0, 4, 8, c }
+    // which lower 2 bits are 00 in binary.
+    assign i_addr_misaligned = current_pc[1:0] != 2'b00;
+
 endmodule
