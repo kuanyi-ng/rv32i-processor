1ac09df86d337ff89f46aaf6b33cdfe4d585dbf8
KY-Ng <raionkurane@gmail.com>
2021-06-04 16:32:11 +0900

    update trap test program (from senpai)

--- test_pack/exception/trap/test.s
+++ test_pack/exception/trap/test.s
@@ -1,30 +1,32 @@
 .text
 main:
-	li x1, 0xf0000000
-	lui x2, %hi(START_MESSAGE)
-	addi x2, x2, %lo(START_MESSAGE)
+	li s1, 0xf0000000
+	lui s2, %hi(START_MESSAGE)
+	addi s2, s2, %lo(START_MESSAGE)
 P_START_MESSAGE:
-	lb 	x3, 0(x2)
-	beqz x3, ECALL
-	sb 	x3, 0(x1)
-	addi x2, x2, 1
+	lb 	s3, 0(s2)
+	beqz s3, ECALL
+	sb 	s3, 0(s1)
+	addi	s2, s2, 1
 	j 	P_START_MESSAGE
 ECALL:	
 	ecall
-	lui x2, %hi(CHECK_PASSED)
-	addi x2, x2, %lo(CHECK_PASSED)
+	lui s2, %hi(CHECK_PASSED)
+	addi s2, s2, %lo(CHECK_PASSED)
 P_END_MESSAGE:
-	lb 	x3, 0(x2)
-	beqz x3, EXIT
-	sb	x3, 0(x1)
-	addi x2, x2, 1
+	lb 	s3, 0(s2)
+	beqz s3, EXIT
+	sb	s3, 0(s1)
+	addi	s2, s2, 1
 	j P_END_MESSAGE
 EXIT:
-	li x1, 0xff000000
-	sw x0, 0(x1)
+	li s1, 0xff000000
+	sw s0, 0(s1)
 
 .section .rodata
+
 START_MESSAGE:
 .string "TRAP TEST\n"
+
 CHECK_PASSED:
 .string "CHECK PASSED!!\n"
