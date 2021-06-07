57985ad8d2502e697206626f95b0b9ba916e801f
KY-Ng <raionkurane@gmail.com>
2021-06-07 16:30:43 +0900

    rename logical_synthesis to logic_synthesis

---
+++ script/run_logic_synthesis.sh
@@ -0,0 +1,7 @@
+# shell
+
+cd logic_synthesis_ground/
+
+dc_shell-t -f logic_synthesis.tcl | tee log
+
+cd ~/sandbox
--- script/run_logical_synthesis.sh
+++
@@ -1,7 +0,0 @@
-# shell
-
-cd logical_synthesis_ground/
-
-dc_shell-t -f logical_synthesis.tcl | tee log
-
-cd ~/sandbox
