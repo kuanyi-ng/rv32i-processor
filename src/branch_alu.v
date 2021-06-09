18c3ac96a5435cb287379d0321109736d3c3e350
KY-Ng <raionkurane@gmail.com>
2021-06-09 13:48:26 +0900

    change implementation of branch_alu

--- src/branch_alu.v
+++ src/branch_alu.v
@@ -17,13 +17,19 @@ module branch_alu (
     // functions
     //
     function branch_alu_out(input [31:0] in1, input [31:0] in2, input [2:0] op);
+        reg equal, lt, ltu;
+
         begin
+            equal = (in1 == in2);
+            lt = ($signed(in1) < $signed(in2));
+            ltu = (in1 < in2);
+
             case (op)
                 // EQ
-                `EQ: branch_alu_out = (in1 == in2);
+                `EQ: branch_alu_out = equal;
 
                 // NE
-                `NE: branch_alu_out = (in1 != in2);
+                `NE: branch_alu_out = !equal;
 
                 // JAL, JALR, MRET
                 `JUMP: branch_alu_out = 1'b1;
@@ -32,19 +38,19 @@ module branch_alu (
                 `NO_JUMP: branch_alu_out = 1'b0;
 
                 // LT
-                `LT: branch_alu_out = ($signed(in1) < $signed(in2));
+                `LT: branch_alu_out = lt;
 
                 // GE
-                `GE: branch_alu_out = ($signed(in1) >= $signed(in2));
+                `GE: branch_alu_out = !lt;
 
                 // LTU
-                `LTU: branch_alu_out = (in1 < in2);
+                `LTU: branch_alu_out = ltu;
 
                 // GEU
-                `GEU: branch_alu_out = (in1 >= in2);
+                `GEU: branch_alu_out = !ltu;
 
                 // no default case as every case is covered
-            endcase 
-        end        
+            endcase
+        end
     endfunction    
 endmodule
