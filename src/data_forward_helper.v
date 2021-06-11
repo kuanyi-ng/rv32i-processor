b23b21f6cd68563df568c2b6086a507e797b239e
KY-Ng <raionkurane@gmail.com>
2021-06-11 13:15:38 +0900

    migrate from opcode to ir_type in data_forward_helper

--- src/data_forward_helper.v
+++ src/data_forward_helper.v
@@ -1,4 +1,4 @@
-`include "constants/opcode.v"
+`include "constants/ir_type.v"
 // check opcode to determine the type of instruction
 //
 // LUI      : x[rd] <- C => main_data
@@ -19,7 +19,7 @@ module data_forward_helper
     input [31:0] main_data,
     input [31:0] sub_data,
     input [31:0] csr_data,
-    input [6:0] opcode,
+    input [3:0] ir_type,
     input [2:0] funct3,
 
     output [31:0] data_to_forward
@@ -32,7 +32,7 @@ module data_forward_helper
         main_data,
         sub_data,
         csr_data,
-        opcode,
+        ir_type,
         funct3,
         IS_MEM_STAGE
     );
@@ -45,36 +45,26 @@ module data_forward_helper
         input [31:0] main,
         input [31:0] sub,
         input [31:0] csr,
-        input [6:0] opcode,
+        input [3:0] ir_type,
         input [2:0] funct3,
         input is_mem_stage
     );
         // NOTE: don't forward data from EX stage
         // if the instruction executing in EX stage is a load instruction.
-        reg is_lui, is_auipc, is_i_type, is_r_type, is_load, is_jal, is_jalr, is_csr;
-
         begin
-            is_lui = (opcode == `LUI_OP);
-            is_auipc = (opcode == `AUIPC_OP);
-            is_i_type = (opcode == `I_TYPE_OP);
-            is_r_type = (opcode == `R_TYPE_OP);
-            is_load = (opcode == `LOAD_OP);
-            is_jal = (opcode == `JAL_OP);
-            is_jalr = (opcode == `JALR_OP);
-            is_csr = (opcode == `SYSTEM_OP) && (funct3 != 3'b000);
+            case (ir_type)
+                `LUI_IR: prep_data_to_forward = main;
+                `AUIPC_IR: prep_data_to_forward = main;
+                `REG_IMM_IR: prep_data_to_forward = main;
+                `REG_REG_IR: prep_data_to_forward = main;
+                `JAL_IR: prep_data_to_forward = sub;
+                `JALR_IR: prep_data_to_forward = sub;
+                `CSR_IR: prep_data_to_forward = csr;
+                `LOAD_IR: prep_data_to_forward = (is_mem_stage) ? sub : 32'bx;
 
-            if (is_lui || is_auipc || is_i_type || is_r_type) begin
-                prep_data_to_forward = main;
-            end else if (is_jal || is_jalr) begin
-                prep_data_to_forward = sub;
-            end else if (is_csr) begin
-                prep_data_to_forward = csr;
-            end else if (is_load) begin
-                prep_data_to_forward = (is_mem_stage) ? sub : 32'bx;
-            end else begin
-                // includes Store, Branch instructions
-                prep_data_to_forward = 32'bx;
-            end
+                // default includes Store, Branch Instruction
+                default: prep_data_to_forward = 32'bx;
+            endcase
         end
     endfunction
 endmodule
