d10492f354112bbd48ef34e7dc6a04eec0235c8c
KY-Ng <raionkurane@gmail.com>
2021-06-14 17:57:28 +0900

    pass jump_prediction and addr_prediction from IF to EX

--- src/if_id_regs.v
+++ src/if_id_regs.v
@@ -22,7 +22,13 @@ module if_id_regs (
     output flush_out,
 
     input i_addr_misaligned_in,
-    output i_addr_misaligned_out
+    output i_addr_misaligned_out,
+
+    input jump_prediction_in,
+    output jump_prediction_out,
+
+    input [31:0] addr_prediction_in,
+    output [31:0] addr_prediction_out
 );
 
     reg [31:0] pc;
@@ -31,6 +37,8 @@ module if_id_regs (
     reg [3:0] ir_type;
     reg flush;
     reg i_addr_misaligned;
+    reg jump_prediction;
+    reg [31:0] addr_prediction;
 
     localparam [31:0] NOP_IR = { 12'b0, 5'b0, 3'b0, 5'b0, 7'b0010011 };
 
@@ -52,6 +60,8 @@ module if_id_regs (
             ir_type <= ir_type;
             flush <= flush;
             i_addr_misaligned <= i_addr_misaligned;
+            jump_prediction <= jump_prediction;
+            addr_prediction <= addr_prediction;
         end else begin
             // update value
             pc <= pc_in;
@@ -60,6 +70,8 @@ module if_id_regs (
             ir_type <= ir_type_in;
             flush <= flush_in;
             i_addr_misaligned <= i_addr_misaligned_in;
+            jump_prediction <= jump_prediction_in;
+            addr_prediction <= addr_prediction_in;
         end
     end
 
@@ -69,5 +81,7 @@ module if_id_regs (
     assign ir_type_out = ir_type;
     assign flush_out = flush;
     assign i_addr_misaligned_out = i_addr_misaligned;
-    
+    assign jump_prediction_out = jump_prediction;
+    assign addr_prediction_out = addr_prediction;
+
 endmodule
